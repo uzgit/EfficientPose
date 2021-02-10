19a236351ae40a7c9c5e0452315d73312dc2a09e
joshua <jspri17@gmail.com>
2021-02-10 15:08:05 +0000

    More modularization of install scripts.

--- install_efficient_pose.sh
+++ install_efficient_pose.sh
@@ -1,15 +1,5 @@
-# conda
-echo "Installing Conda..."
-MINICONDA_INSTALLER_SCRIPT=Miniconda3-4.5.4-Linux-x86_64.sh
-MINICONDA_PREFIX=/usr/local
-wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER_SCRIPT
-chmod +x $MINICONDA_INSTALLER_SCRIPT
-./$MINICONDA_INSTALLER_SCRIPT -b -f -p $MINICONDA_PREFIX
-
 # efficientpose
 echo "Installing EfficientPose..."
-git clone https://github.com/uzgit/EfficientPose
-cd EfficientPose/
 conda create -n -y EfficientPose python==3.7
 conda activate EfficientPose
 conda install -y tensorflow-gpu==1.15.0
